85dfb405c048b7beebe9289da059b651ab4f3ad3
¨KasperHdL¨ <kasper@honnens.dk>
2016-03-23 14:05:55 +0100

    Simple Floating Objects and stupidly simple boat control

---
+++ Assets/Seat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seat : MonoBehaviour {
+    
+    [SerializeField]
+    private BoatController boat;
+    
+    
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    
+    void OnTriggerStay(Collider coll){
+        if(coll.gameObject.tag == "Player"){
+            PlayerInteract player = coll.GetComponent<PlayerInteract>();
+            if(player.buttonDown){
+                player.StartInteraction(boat);
+            }
+        }
+    }
+}
