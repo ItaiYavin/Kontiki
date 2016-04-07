fb309b5538f45b13b0e0ac872defccc99305305e
Frans Larsen <larsen.frans@gmail.com>
2016-04-07 02:17:20 +0200

    Basic Inventory using Key Inputs
    Alpha 1-n (n = inventory size)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+namespace Kontiki { 
+    public class Inventory : MonoBehaviour {
+        [Range(4, 9)]
+        public int inventorySize = 4;
+
+        [SerializeField]
+        private EdibleItem[] _inventoryItems;
+        private Character _character;
+
+	    void Start () {
+            _inventoryItems = new EdibleItem[inventorySize];
+            _character = GetComponent<Character>();
+        }
+	
+	    void Update () {
+            CheckIfInventoryItemIsPressed();
+
+        }
+
+        //TODO Find better name
+        void CheckIfInventoryItemIsPressed()
+        {
+            for(int i = 1; i <= inventorySize; i++)
+            {
+                if(Input.GetKeyUp(""+i))
+                {
+                    UseInventoryItem(i-1); // Zero index start
+                }
+            }
+        }
+
+        void UseInventoryItem(int i)
+        {
+            if (_inventoryItems[i] == null) return;
+
+            _character.ConsumeEdibleItem(_inventoryItems[i]);
+            _inventoryItems[i] = null;
+        }
+    }
+}
