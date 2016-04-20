a06486fb657d37761289c7aa1c0e749c9c7a2d4b
Itai Yavin <Itai.yavin@gmail.com>
2016-04-20 09:37:46 +0200

    added isMoving scorer WIP added pickUpItem action WIP

---
+++ Assets/Scripts/AI/Character/Actions/PickUpItem.cs
@@ -0,0 +1,30 @@
+﻿using Apex.AI;
+using Apex.Serialization;
+using Kontiki;
+using UnityEngine;
+
+namespace Kontiki.AI{
+    /// <summary>
+    /// Picks up item into the inventory (if not already there)
+    /// </summary>
+    /// <seealso cref="Apex.AI.ActionBase" />
+	public class PickUpItem : ActionBase {
+		enum ItemType 
+		{
+			edible, 
+			fuel
+		};
+
+		[ApexSerialization, FriendlyName("Debug", "if on, writes debug messages to console")]
+		bool debug;
+
+		[ApexSerialization, FriendlyName("Item Types", "Type of item")]
+		ItemType itemType;
+
+		public override void Execute(IAIContext context){
+			/*switch(){
+				Case :
+			}*/
+		}
+	}
+}
