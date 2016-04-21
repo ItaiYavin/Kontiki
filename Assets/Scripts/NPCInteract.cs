e8f9df9ec2269edf5ab09c312e78a0d68dae6be4
KasperHdL <kasper@honnens.dk>
2016-04-21 16:34:46 +0200

    Final Test Scene

---
+++ Assets/Scripts/NPCInteract.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kontiki
+{
+		
+	public sealed class NPCInteract : Interactable {
+		
+		bool jumping = false;
+		float nextJump = 0;
+		float jumpLength = 1f;
+		float jumpForce = 200f;
+		
+		Rigidbody body;
+		
+		void Awake(){
+			body = GetComponent<Rigidbody>();
+		}
+		
+		void FixedUpdate(){
+			if(jumping && Time.time > nextJump){
+				body.AddForce(Vector3.up*jumpForce);
+				nextJump = Time.time + jumpLength;
+			}
+		}
+		
+        public override void Interact(Character character){
+			int inventorySize = character.GetInventory().inventorySize;
+			if(inventorySize == 0){
+				Debug.Log("You have no items!");
+				return;
+			}
+			for(int i = 0; i < inventorySize; i++){
+				if(character.GetInventory().GetInventoryItem(i) is EdibleItem){
+					character.GetInventory().GetInventoryItems()[i] = null;
+					Debug.Log("YAY, thanks for the apple!");
+					jumping = true;
+					return;
+				}
+			}
+			
+			Debug.Log("You have no edible items!");
+	    }
+	}
+}
---
+++ Assets/Scripts/StatImage.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Kontiki;
+
+public class StatImage : MonoBehaviour {
+    [Range(0.1f, 1f)]
+	public float widthRatio;
+    [Range(0.1f, 1f)]
+	public float heightRatio;
+
+	public float minSize;
+	public float maxSize;
+
+	public float cornerMargin;
+
+    [Range(0.1f, 1f)]
+	public float minTransparency;
+    [Range(0.1f, 1f)]
+	public float maxTransparency;
+
+	public Character character;
+	
+	private Image _image;
+	private Color _imageCol;
+	private RectTransform _rt;
+
+	private Rect _rect;
+	private Vector2 _position;
+	private Vector2 _originalSize;
+	
+	public enum Type 
+	{
+		Hunger,
+		Thirst
+	};
+
+	public Type statType;
+	
+	// Use this for initialization
+	void Start () {
+		_image = GetComponent<Image>();
+		_imageCol = _image.color;
+		_rt = _image.GetComponent<RectTransform>();
+
+		_position = _rect.position;
+		_originalSize = _rect.size;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		SetImageAccordingToStatType();
+	}
+
+	public void SetImageAccordingToStatType(){
+		float percent = 0;
+
+		switch(statType){
+			case Type.Hunger:
+			percent = character.hunger / Character.hungerMax;
+			break;
+
+			case Type.Thirst:
+			break;
+
+			default:
+			break;
+		}
+		float newSize = minSize+percent*(maxSize-minSize);
+		_rt.sizeDelta = new Vector2(newSize*widthRatio, newSize*heightRatio);
+		float newTrans = minTransparency+(percent*(maxTransparency-minTransparency));
+		_image.color = new Color(_imageCol.r, _imageCol.b, _imageCol.g, newTrans);
+
+		CalculateImagePosition();
+	}
+
+	public void CalculateImagePosition(){
+		Vector2 positionVector = new Vector2(_rt.rect.width + _rt.rect.position.x + cornerMargin, _rt.rect.height + _rt.rect.position.y + cornerMargin);
+		Rect tempRect = _rt.rect;
+		tempRect.position = positionVector;
+		_rt.position = tempRect.position;
+	}
+}
