4987026f2a3cdbf2f1cdab04722297d3e7d54bfa
Frans Peter Larsen <larsen.frans@gmail.com>
2016-04-29 13:43:57 +0200

    Icons for items (Not done) and missing commit file

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -2,7 +2,20 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class InventoryItem : MonoBehaviour {
-
+public class InventoryItem : MonoBehaviour
+{
 	public Text text;
+
+    public Texture2D iconTexture;
+    public Material iconMaterial
+    {
+        get;
+        private set;
+    }
+
+    void Start()
+    {
+        iconMaterial = new Material(Shader.Find("Sprites/Default"));
+        iconMaterial.SetTexture("_MainTex", iconTexture);
+    }
 }
